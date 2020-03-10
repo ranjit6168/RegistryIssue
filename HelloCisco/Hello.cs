5fb321e066a5e70aff83c6ae31f7deafb55adfe7
rjamdar <rjamdar@cisco.com>
2020-03-10 14:44:40 +0530

    Add project files.

---
+++ HelloCisco/Hello.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Management.Automation;
+using Microsoft.Win32;
+
+namespace HelloCisco
+{
+    [Cmdlet(VerbsCommon.Get, "Path")]
+    [OutputType(typeof(String))]
+    public class GetHello : Cmdlet
+    {
+
+        [Parameter(Position = 1, ValueFromPipelineByPropertyName = true)]
+        public string Name { get; set; }
+
+       protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+        }
+
+        protected override void ProcessRecord()
+        {
+            string javapath = String.Empty;
+            string path = String.Empty;
+            using (Microsoft.Win32.RegistryKey rk = Microsoft.Win32.Registry.LocalMachine.OpenSubKey("SOFTWARE\\Wow6432Node\\JavaSoft\\Java Runtime Environment\\"))
+            {
+                if (rk != null)
+                {
+                    string currentVersion = rk.GetValue("CurrentVersion").ToString();
+                    if (currentVersion != null)
+                    {
+                        using (Microsoft.Win32.RegistryKey key = rk.OpenSubKey(currentVersion))
+                        {
+                            if (key != null)
+                                path = key.GetValue("JavaHome").ToString();
+                        }
+                    }
+                }
+            }
+            if (Name != null)
+            {
+                javapath += path;
+            }
+
+            WriteObject(javapath);
+        }
+
+    }
+}
